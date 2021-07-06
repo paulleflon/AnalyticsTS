7ec4f0551ddc31ffc80e01db12873e252d27ace7
Hicka <hello@hicka.dev>
2021-07-07 01:25:22 +0200

    feat: ceate base file

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import { Intents, Snowflake } from 'discord.js';
+import { config as dotenvConfig } from 'dotenv';
+dotenvConfig({ path: '../.env' });
+import BotClient from './core/BotClient';
+const client: BotClient = new BotClient({
+	admins: process.env.ADMINS?.split(',').filter(id => /(\d){17,19}/.test(id)) as Snowflake[],
+	owner: process.env.OWNER! as Snowflake,
+	prefix: process.env.GLOBAL_PREFIX!,
+	intents: Object.values(Intents.FLAGS), // Taking all intents for now. Should choose them later on
+	restTimeOffset: 0,
+	token: process.env.DISCORD_TOKEN!,
+	test: process.env.TEST === 'true'
+});
+
+client.start();
